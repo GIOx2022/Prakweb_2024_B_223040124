ae1d72d58ef61123efd771d2322a782d964e000d
GIOx2022 <119664509+GIOx2022@users.noreply.github.com>
2024-10-22 10:28:01 +0700

    Add files via upload

---
+++ MVC/app/config/config.php
@@ -0,0 +1,9 @@
+<?php  
+
+define('BASEURL', 'http://localhost/Praktikum%20Web/Prakweb2024_B_223040124_PW/MVC/public/');
+
+//DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'phpmvc');
---
+++ MVC/app/models/User_model.php
@@ -0,0 +1,10 @@
+<?php 
+
+class user_model {
+    private $nama = 'GIo';
+
+    public function getUser() {
+        return $this->nama;
+    }
+}
+?>
